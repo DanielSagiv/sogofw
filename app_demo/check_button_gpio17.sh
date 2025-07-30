a83e2714d1ef75f59eaece9bffa27c36f73eafeb
SagivDaniel+1-at-345037243928 <77956838+DanielSagiv@users.noreply.github.com>
2025-07-30 17:34:27 +0300

    fix 11

--- app_demo/check_button_gpio17.sh
+++ app_demo/check_button_gpio17.sh
@@ -7,9 +7,9 @@ echo "📟 Monitoring GPIO $LINE on $CHIP (Press Ctrl+C to exit)"
 
 while true; do
     val=$(gpioget $CHIP $LINE 2>/dev/null)
-    if [ "$val" = "0" ]; then
+    if [ "$val" = "1" ]; then
         echo "🔴 Button Pressed"
-    elif [ "$val" = "1" ]; then
+    elif [ "$val" = "0" ]; then
         echo "⚪️ Button Not Pressed"
     else
         echo "⚠️ GPIO read error or unknown state: '$val'"
