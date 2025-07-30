95b8e782d23e3e97bb7793f3c5c863464c9a27fc
SagivDaniel+1-at-345037243928 <77956838+DanielSagiv@users.noreply.github.com>
2025-07-30 17:45:27 +0300

    fix 13

--- app_demo/check_button_gpio17.sh
+++ app_demo/check_button_gpio17.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-
 echo "📟 Monitoring GPIO 17 on gpiochip0 (Press Ctrl+C to exit)"
-
-# Set GPIO 17 as input with pull-up
-gpioset gpiochip0 17=tri-hi
-
 while true; do
     val=$(gpioget gpiochip0 17 2>/dev/null)
-    if [ "$val" == "0" ]; then
+    if [ "$val" = "0" ]; then
         echo "🔴 Button Pressed"
     else
         echo "⚪ Button Not Pressed"
     fi
-    sleep 0.3
+    sleep 0.2
 done
